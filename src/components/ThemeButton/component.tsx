6747608341b7230e4c5112b2a776aea618f64026
yauhenimalakhouski <122909276+yauhenimalakhouski@users.noreply.github.com>
2023-10-28 16:43:58 +0300

    theme button ready

--- src/components/ThemeButton/component.tsx
+++ src/components/ThemeButton/component.tsx
@@ -3,40 +3,31 @@
 import { useEffect, useState } from "react";
 import { Button } from "../Button/component";
 
-const LOCAL_STORAGE_KEY = "theme";
-
-let themeLocal: string | null;
+const LOCAL_STORAGE_KEY:string = "theme";
+let themeLocal;
 
 export const ThemeButton = () => {
-    const [theme, setTheme] = useState("dark");
-    useEffect(() => {
-      if (typeof window !== "undefined") {
-        themeLocal = localStorage.getItem(LOCAL_STORAGE_KEY);
-        if (themeLocal) {
-          setTheme(themeLocal);
-        }
-      }
+  const [theme, setTheme] = useState<string>("dark");
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      themeLocal = localStorage.getItem(LOCAL_STORAGE_KEY);
       if (themeLocal) {
-        
-          setTheme(themeLocal);
-        }
-      
-    }, []);
-  
-    useEffect(() => {
-      if (typeof window !== "undefined") {
-        localStorage.setItem(LOCAL_STORAGE_KEY, theme);
+        setTheme(themeLocal);
       }
-    }, [theme]);
+    }
+  }, []);
 
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      localStorage.setItem(LOCAL_STORAGE_KEY, theme);
+    }
+  }, [theme]);
 
   return (
     <Button
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
-      type={"btn_theme"}
-    >
-      {theme}
-    </Button>
+      type={`btn_theme`}
+      switchType={`${theme}`}
+    >{theme}</Button>
   );
 };
-
