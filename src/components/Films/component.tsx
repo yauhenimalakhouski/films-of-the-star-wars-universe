a507e2bd7701aacac3aeff7b15c94d83776e6643
yauhenimalakhouski <122909276+yauhenimalakhouski@users.noreply.github.com>
2023-11-01 23:10:46 +0300

    filmsView buttons added

--- src/components/Films/component.tsx
+++ src/components/Films/component.tsx
@@ -1,13 +1,20 @@
 import { Film } from "@/types/types";
 import { Movie } from "../Movie/component";
 
+import stylesComponent from "./stylesComponent.module.css"
+import classNames from "classnames";
+
 type CompProps = {
   films: Film[];
+  filmsView: string;
 };
 
-export const Films = ({ films }: CompProps) => {
+export const Films = ({ films, filmsView }: CompProps) => {
   return (
-    <div>
+    <div className={classNames(stylesComponent.root, {
+      [stylesComponent.table]: filmsView === "table",
+      [stylesComponent.tile]: filmsView === "tile",
+    })}>
       {films.map((film) => (
         <Movie key={film.id} film={film} />
       ))}
--- src/components/Films/container.tsx
+++ src/components/Films/container.tsx
@@ -2,11 +2,14 @@
 import { useGetFilmsQuery } from "@/store/services/api";
 import { Films } from "./component";
 import { getSortFn } from "@/utils/sotrFn";
-import { loadSortTypeFromLocalStorage } from "@/utils/localStorage";
+
 import { useAppSelector } from "@/store/reduxHooks/reduxHooks";
 import { selectSearchFilmsValue } from "@/store/features/searchFilms/selector";
 import { useEffect, useState } from "react";
 import { selectSortTypeValue } from "@/store/features/sortType/selectors";
+import { Button } from "../Button/component";
+
+import styles from "./styles.module.css";
 
 export const FilmsContainer = () => {
   const { data: films, isFetching, isSuccess } = useGetFilmsQuery(undefined);
@@ -14,6 +17,7 @@ export const FilmsContainer = () => {
   const searchTextFromStore = useAppSelector(selectSearchFilmsValue);
   const sortTypeValue = useAppSelector(selectSortTypeValue);
   const [searchText, setSearchText] = useState(searchTextFromStore);
+  const [filmsView, setFilmsView] = useState<string>('tile');
 
   useEffect(() => {
     setSearchText(searchTextFromStore);
@@ -34,5 +38,23 @@ export const FilmsContainer = () => {
 
   const currentFilmsSorted = filteredFilms.sort(sortTypeFn);
 
-  return <Films films={currentFilmsSorted}></Films>;
+  return (
+    <div>
+      <div>
+        <div>
+          <Button onClick={()=>{
+            if(filmsView !== "table"){
+              setFilmsView("table")
+            }
+          }}>table</Button>
+          <Button onClick={()=>{
+            if(filmsView !== "tile"){
+              setFilmsView("tile")
+            }
+          }}>tile</Button>
+        </div>
+      </div>
+      <Films films={currentFilmsSorted} filmsView = {filmsView}></Films>;
+    </div>
+  );
 };
